be2f8397c8316bd34b50068574bf3d51296798d6
Alia Collins <aliacollinseve@gmail.com>
2026-02-26 23:05:38 +0530

    Alpha 1.0.0-alpha.3: Merge Plan Editor sidebar: combine Summary + Optimize into unified Plan tab with inline optimization, remap context menu, and manual attribute dialog

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -30,9 +30,9 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision (0 for stable, 1+ for beta)
 //
-[assembly: AssemblyVersion("1.0.0.2")]
-[assembly: AssemblyFileVersion("1.0.0.2")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha.2")]
+[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.3")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha.3")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
