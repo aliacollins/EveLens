5411505fd4f0602c3020129e645fde8f00875798
Alia Collins <aliacollinseve@gmail.com>
2026-02-27 11:19:06 +0530

    Alpha 1.0.0-alpha.15: Fix settings never saving on Linux/macOS: SmartSettingsManager was deadlocking on non-Windows dispatchers

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -30,9 +30,9 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision (0 for stable, 1+ for beta)
 //
-[assembly: AssemblyVersion("1.0.0.14")]
-[assembly: AssemblyFileVersion("1.0.0.14")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha.14")]
+[assembly: AssemblyVersion("1.0.0.15")]
+[assembly: AssemblyFileVersion("1.0.0.15")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha.15")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
