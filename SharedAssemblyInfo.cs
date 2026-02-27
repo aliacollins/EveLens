9fb81626569b4a89d73065c252752c333d18cd0b
Alia Collins <aliacollinseve@gmail.com>
2026-02-27 10:49:11 +0530

    Alpha 1.0.0-alpha.12: Fix macOS .app.zip: proper bundle structure with ad-hoc codesigning

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -30,9 +30,9 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision (0 for stable, 1+ for beta)
 //
-[assembly: AssemblyVersion("1.0.0.11")]
-[assembly: AssemblyFileVersion("1.0.0.11")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha.11")]
+[assembly: AssemblyVersion("1.0.0.12")]
+[assembly: AssemblyFileVersion("1.0.0.12")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha.12")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
