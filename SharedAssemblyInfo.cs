b5fa2e769601f74126c23f65ba0790e55f695b9d
Alia Collins <aliacollinseve@gmail.com>
2026-02-27 16:39:07 +0530

    Alpha 1.0.0-alpha.20: Fix settings crash on Linux/macOS: System.Drawing.Common not supported, use raw ARGB values

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -30,9 +30,9 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision (0 for stable, 1+ for beta)
 //
-[assembly: AssemblyVersion("1.0.0.19")]
-[assembly: AssemblyFileVersion("1.0.0.19")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha.19")]
+[assembly: AssemblyVersion("1.0.0.20")]
+[assembly: AssemblyFileVersion("1.0.0.20")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha.20")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
