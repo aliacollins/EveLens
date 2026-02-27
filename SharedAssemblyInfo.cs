dc02bad73309002d2701c3f147554be43e0f6a4e
Alia Collins <aliacollinseve@gmail.com>
2026-02-27 10:26:08 +0530

    Alpha 1.0.0-alpha.9: Fix Linux settings loss: save in Closing handler before window closes instead of relying on ShutdownRequested

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -30,9 +30,9 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision (0 for stable, 1+ for beta)
 //
-[assembly: AssemblyVersion("1.0.0.8")]
-[assembly: AssemblyFileVersion("1.0.0.8")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha.8")]
+[assembly: AssemblyVersion("1.0.0.9")]
+[assembly: AssemblyFileVersion("1.0.0.9")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha.9")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
