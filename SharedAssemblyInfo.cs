21e44f8059774b3cad0f9828e14e5bb87e9f85d3
Alia Collins <aliacollinseve@gmail.com>
2026-02-28 10:04:55 +0530

    Alpha 1.0.0-alpha.24: Fix settings crash: defer CloudStorageServiceProvider loading that triggered System.Drawing on Linux/macOS

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -30,9 +30,9 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision (0 for stable, 1+ for beta)
 //
-[assembly: AssemblyVersion("1.0.0.23")]
-[assembly: AssemblyFileVersion("1.0.0.23")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha.23")]
+[assembly: AssemblyVersion("1.0.0.24")]
+[assembly: AssemblyFileVersion("1.0.0.24")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha.24")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
