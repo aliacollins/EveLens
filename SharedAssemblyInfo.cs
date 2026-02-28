d0a53bb06e5b5fa8080da8d07e588aafa5b97ffe
Alia Collins <aliacollinseve@gmail.com>
2026-02-28 17:45:03 +0530

    Alpha 1.0.0-alpha.27: Per-component character folder storage

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -30,9 +30,9 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision (0 for stable, 1+ for beta)
 //
-[assembly: AssemblyVersion("1.0.0.26")]
-[assembly: AssemblyFileVersion("1.0.0.26")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha.26")]
+[assembly: AssemblyVersion("1.0.0.27")]
+[assembly: AssemblyFileVersion("1.0.0.27")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha.27")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
