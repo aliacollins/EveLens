484dff94d3cbac83c59a230023dde222fd7bd7fe
Alia Collins <aliacollinseve@gmail.com>
2026-03-16 00:40:38 +0530

    Alpha 1.0.0-alpha.35: Feedback round 2: ESI resilience pipeline, collapse persistence, auto-update, notification fixes

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -30,9 +30,9 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision (0 for stable, 1+ for beta)
 //
-[assembly: AssemblyVersion("1.0.0.34")]
-[assembly: AssemblyFileVersion("1.0.0.34")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha.34")]
+[assembly: AssemblyVersion("1.0.0.35")]
+[assembly: AssemblyFileVersion("1.0.0.35")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha.35")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
