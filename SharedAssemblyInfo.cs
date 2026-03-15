f96bc7424cac41dea8434430ed39419b2732e141
Alia Collins <aliacollinseve@gmail.com>
2026-03-15 18:52:43 +0530

    Alpha 1.0.0-alpha.30: Projection Overlay Engine: 43% memory reduction, virtualized rendering, collapse persistence

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -30,9 +30,9 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision (0 for stable, 1+ for beta)
 //
-[assembly: AssemblyVersion("1.0.0.29")]
-[assembly: AssemblyFileVersion("1.0.0.29")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha.29")]
+[assembly: AssemblyVersion("1.0.0.30")]
+[assembly: AssemblyFileVersion("1.0.0.30")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha.30")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
