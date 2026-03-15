b7a8a4c1b469a218cc90750f60b1b84e66a89deb
Alia Collins <aliacollinseve@gmail.com>
2026-03-15 21:01:33 +0530

    Alpha 1.0.0-alpha.31: Issue #17 complete: tray tooltip, clone tracking, tab loading overlay, character reorder in groups

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -30,9 +30,9 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision (0 for stable, 1+ for beta)
 //
-[assembly: AssemblyVersion("1.0.0.30")]
-[assembly: AssemblyFileVersion("1.0.0.30")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha.30")]
+[assembly: AssemblyVersion("1.0.0.31")]
+[assembly: AssemblyFileVersion("1.0.0.31")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha.31")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
