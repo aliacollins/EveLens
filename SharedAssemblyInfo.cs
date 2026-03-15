486142dd77afe249adc6871c317cebb71d957acd
Alia Collins <aliacollinseve@gmail.com>
2026-03-15 15:51:52 +0530

    Alpha 1.0.0-alpha.29: Issue #17 fixes: memory leaks, race condition, DateTime crash protection

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -30,9 +30,9 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision (0 for stable, 1+ for beta)
 //
-[assembly: AssemblyVersion("1.0.0.28")]
-[assembly: AssemblyFileVersion("1.0.0.28")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha.28")]
+[assembly: AssemblyVersion("1.0.0.29")]
+[assembly: AssemblyFileVersion("1.0.0.29")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha.29")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
