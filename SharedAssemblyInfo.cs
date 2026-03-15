5b2ca1d5d590c628b206afa4741745df13907d48
Alia Collins <aliacollinseve@gmail.com>
2026-03-15 22:02:06 +0530

    Alpha 1.0.0-alpha.34: Fix clipboard deadlock on Linux/macOS, async clipboard API

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -30,9 +30,9 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision (0 for stable, 1+ for beta)
 //
-[assembly: AssemblyVersion("1.0.0.33")]
-[assembly: AssemblyFileVersion("1.0.0.33")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha.33")]
+[assembly: AssemblyVersion("1.0.0.34")]
+[assembly: AssemblyFileVersion("1.0.0.34")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha.34")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
