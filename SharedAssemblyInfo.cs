4941c13e5bc43beeb6352ed94c98e7f4e1f295c8
Alia Collins <aliacollinseve@gmail.com>
2026-03-15 21:52:26 +0530

    Alpha 1.0.0-alpha.33: Update feed separation, Linux desktop integration, About cleanup

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -30,9 +30,9 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision (0 for stable, 1+ for beta)
 //
-[assembly: AssemblyVersion("1.0.0.32")]
-[assembly: AssemblyFileVersion("1.0.0.32")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha.32")]
+[assembly: AssemblyVersion("1.0.0.33")]
+[assembly: AssemblyFileVersion("1.0.0.33")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha.33")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
