9c0def99ae94eaab9d601fff24b1fbf83a3036c9
Alia Collins <aliacollinseve@gmail.com>
2026-03-16 18:19:54 +0530

    Alpha 1.0.0-alpha.42: Beta launch prep: scrollbar fix, window position save, tray toggle, group collapse/reorder, README rewrite

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -30,9 +30,9 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision (0 for stable, 1+ for beta)
 //
-[assembly: AssemblyVersion("1.0.0.41")]
-[assembly: AssemblyFileVersion("1.0.0.41")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha.41")]
+[assembly: AssemblyVersion("1.0.0.42")]
+[assembly: AssemblyFileVersion("1.0.0.42")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha.42")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
