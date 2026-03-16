2943d94bb359d253e7e71c7ed60bad75d9574c0a
Alia Collins <aliacollinseve@gmail.com>
2026-03-16 18:58:43 +0530

    Alpha 1.0.0-alpha.43: Fix window position save/restore across restarts and multi-monitor setups

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -30,9 +30,9 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision (0 for stable, 1+ for beta)
 //
-[assembly: AssemblyVersion("1.0.0.42")]
-[assembly: AssemblyFileVersion("1.0.0.42")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha.42")]
+[assembly: AssemblyVersion("1.0.0.43")]
+[assembly: AssemblyFileVersion("1.0.0.43")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha.43")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
