b57ee02be2540711b32a9bb230436db79dab491c
Alia Collins <aliacollinseve@gmail.com>
2026-03-19 18:14:40 +0530

    Beta 1.0.0-beta.2: ESI health state machine, SDE Catalyst expansion, debug isolation, version counter fix

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -30,9 +30,9 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision (0 for stable, 1+ for beta)
 //
-[assembly: AssemblyVersion("1.0.0.47")]
-[assembly: AssemblyFileVersion("1.0.0.47")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha.47")]
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
+[assembly: AssemblyInformationalVersion("1.0.0-beta.2")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
