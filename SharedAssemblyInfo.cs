ada17872b0bc4380571671072d245d014efa6a42
Alia Collins <aliacollinseve@gmail.com>
2026-03-28 12:39:56 +0530

    Fix version to 1.1.0 series (was incorrectly 1.0.1)

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -30,9 +30,9 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision (0 for stable, 1+ for beta)
 //
-[assembly: AssemblyVersion("1.0.1.4")]
-[assembly: AssemblyFileVersion("1.0.1.4")]
-[assembly: AssemblyInformationalVersion("1.0.1-alpha.4")]
+[assembly: AssemblyVersion("1.1.0.4")]
+[assembly: AssemblyFileVersion("1.1.0.4")]
+[assembly: AssemblyInformationalVersion("1.1.0-alpha.4")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
