faef33c90a0ee808385dfa51adbf2b7935cf08d3
Alia Collins <aliacollinseve@gmail.com>
2026-03-28 11:18:35 +0530

    Alpha 1.0.1-alpha.2: Variable font scaling: 895 hardcoded sizes replaced with 7-tier DynamicResource scale, settings slider 80-150%

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+- Variable font scaling: 895 hardcoded sizes replaced with 7-tier DynamicResource scale, settings slider 80-150%
 - Queue monitor, add character UX, group redesign, blueprint tree, dead code removal (#39, #40, #41, #42, #43)
 
 ### Added
--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -30,9 +30,9 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision (0 for stable, 1+ for beta)
 //
-[assembly: AssemblyVersion("1.0.1.1")]
-[assembly: AssemblyFileVersion("1.0.1.1")]
-[assembly: AssemblyInformationalVersion("1.0.1-alpha.1")]
+[assembly: AssemblyVersion("1.0.1.2")]
+[assembly: AssemblyFileVersion("1.0.1.2")]
+[assembly: AssemblyInformationalVersion("1.0.1-alpha.2")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
