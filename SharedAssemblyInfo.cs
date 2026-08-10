63ef453e201818b58fb83eeaa7781e515235ecd1
Alia Collins <aliacollinseve@gmail.com>
2026-08-10 14:12:50 +0530

    Beta 1.4.0-beta.6: 1.4.0-beta.5: community fix train, SDE 3458726, Attribute Optimizer, EVE Accuracy Suite, auto-update opt-in

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -30,9 +30,9 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision (0 for stable, 1+ for beta)
 //
-[assembly: AssemblyVersion("1.4.0.5")]
-[assembly: AssemblyFileVersion("1.4.0.5")]
-[assembly: AssemblyInformationalVersion("1.4.0-alpha.5")]
+[assembly: AssemblyVersion("1.4.0.6")]
+[assembly: AssemblyFileVersion("1.4.0.6")]
+[assembly: AssemblyInformationalVersion("1.4.0-beta.6")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
