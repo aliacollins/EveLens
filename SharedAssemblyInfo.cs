290e9a22176a51af1bec1d4be68f90153510a540
Alia Collins <aliacollinseve@gmail.com>
2026-08-26 14:44:41 +0530

    Beta 1.5.0-beta.2: Beta 2: SKINR studio public debut, Plan Editor rebuilt, global remap optimization, per-account Skill Farm economics, community fixes

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -30,9 +30,9 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision (0 for stable, 1+ for beta)
 //
-[assembly: AssemblyVersion("1.5.0.3")]
-[assembly: AssemblyFileVersion("1.5.0.3")]
-[assembly: AssemblyInformationalVersion("1.5.0-alpha.3")]
+[assembly: AssemblyVersion("1.5.0.2")]
+[assembly: AssemblyFileVersion("1.5.0.2")]
+[assembly: AssemblyInformationalVersion("1.5.0-beta.2")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
