986b989ea72b404e2e1b03b1a7516fbce4408136
Alia Collins <aliacollinseve@gmail.com>
2026-08-28 11:19:00 +0530

    Beta 1.5.0-beta.15: macOS signing entitlements fix; Velopack update-path verification

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -30,9 +30,9 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision (0 for stable, 1+ for beta)
 //
-[assembly: AssemblyVersion("1.5.0.14")]
-[assembly: AssemblyFileVersion("1.5.0.14")]
-[assembly: AssemblyInformationalVersion("1.5.0-beta.14")]
+[assembly: AssemblyVersion("1.5.0.15")]
+[assembly: AssemblyFileVersion("1.5.0.15")]
+[assembly: AssemblyInformationalVersion("1.5.0-beta.15")]
 
 // Neutral Language
 [assembly: NeutralResourcesLanguage("en-US")]
